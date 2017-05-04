7a5bd404bd167d96b14b45bf0222a85da1adb935
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-04 16:44:35 -0700

    Add sources and layers to map

--- js/bundle.js
+++ js/bundle.js
@@ -567,9 +567,23 @@ window.addEventListener("DOMContentLoaded", function () {
   _mapboxGl2.default.accessToken = 'pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g';
   var map = new _mapboxGl2.default.Map({
     container: 'map',
-    // style: 'mapbox://styles/mapbox/satellite-v9',
-    style: 'mapbox://styles/mapbox/mars-terrain-v1'
+    style: 'mapbox://styles/mapbox/satellite-v9'
   });
+
+  map.on('load', function () {
+    map.addSource('mars-terrain-v1', {
+      "type": 'vector',
+      "url": 'mapbox://mapbox.mars-terrain-v1'
+    });
+
+    map.addLayer({
+      "id": "mars-terrain-v1",
+      "source": "mars-terrain-v1",
+      "type": "raster"
+    });
+  });
+
+  window.map = map;
 });
 
 /***/ })
--- js/mars.js
+++ js/mars.js
@@ -4,7 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g';
   const map = new mapboxgl.Map({
     container: 'map',
-    // style: 'mapbox://styles/mapbox/satellite-v9',
-    style: 'mapbox://styles/mapbox/mars-terrain-v1'
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    // style: 'mapbox://styles/mapbox/mars-terrain-v1'
   });
+
+  map.on('load', () => {
+    map.addSource('mars-terrain-v1', {
+      "type": 'vector',
+      "url": 'mapbox://mapbox.mars-terrain-v1'
+    });
+
+    map.addLayer({
+      "id": "mars-terrain-v1",
+      "source": "mars-terrain-v1",
+      "type": "raster"
+    });
+  });
+
+  window.map = map;
 });
