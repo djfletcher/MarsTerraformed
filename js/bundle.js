351ac52d4d34fed269ce4cc450f0e247fe3f96be
Daniel <daniel.j.fletcher2@gmail.com>
2017-05-08 18:29:17 -0700

    Raster satellite baselayer working with vector terrain layer overtop

--- js/bundle.js
+++ js/bundle.js
@@ -567,22 +567,43 @@ window.addEventListener("DOMContentLoaded", function () {
   _mapboxGl2.default.accessToken = 'pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g';
   var map = new _mapboxGl2.default.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-v9'
+    style: {
+      "version": 8,
+      "sources": {
+        "mars-satellite": {
+          "type": "raster",
+          "url": "mapbox://mapbox.mars-satellite",
+          "tileSize": 256
+        }
+      },
+      "layers": [{
+        "id": "satellite",
+        "type": "raster",
+        "source": "mars-satellite",
+        "minzoom": 0,
+        "maxzoom": 22
+      }]
+    }
+
   });
 
   map.on('load', function () {
-    map.addSource('mars-terrain-v1', {
-      "type": 'vector',
-      "url": 'mapbox://mapbox.mars-terrain-v1'
-    });
-
     map.addLayer({
-      "id": "mars-terrain-v1",
-      "source": "mars-terrain-v1",
-      'layout': {
-        'visibility': 'visible'
+      "id": "terrain",
+      "type": "line",
+      "source": {
+        type: 'vector',
+        url: 'mapbox://mapbox.mars-terrain-v1'
       },
-      "type": "background"
+      "source-layer": "contour",
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "#8B0000",
+        "line-width": 2
+      }
     });
   });
 
--- js/mars.js
+++ js/mars.js
@@ -4,23 +4,43 @@ window.addEventListener("DOMContentLoaded", () => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiZGpmbGV0Y2hlciIsImEiOiJjajF6bjR5djUwMzQzMndxazY3cnR5MGtmIn0.EhgTpiAXtQ6D0H82S24b5g';
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-v9',
-    // style: 'mapbox://styles/mapbox/mars-terrain-v1'
+    style: {
+      "version": 8,
+      "sources": {
+        "mars-satellite": {
+          "type": "raster",
+          "url": "mapbox://mapbox.mars-satellite",
+          "tileSize": 256
+        },
+      },
+      "layers": [{
+        "id": "satellite",
+        "type": "raster",
+        "source": "mars-satellite",
+        "minzoom": 0,
+        "maxzoom": 22
+      }]
+    }
+
   });
 
   map.on('load', () => {
-    map.addSource('mars-terrain-v1', {
-      "type": 'vector',
-      "url": 'mapbox://mapbox.mars-terrain-v1'
-    });
-
     map.addLayer({
-      "id": "mars-terrain-v1",
-      "source": "mars-terrain-v1",
-      'layout': {
-          'visibility': 'visible'
-        },
-      "type": "background"
+      "id": "terrain",
+      "type": "line",
+      "source": {
+        type: 'vector',
+        url: 'mapbox://mapbox.mars-terrain-v1'
+      },
+      "source-layer": "contour",
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "#8B0000",
+        "line-width": 2
+      }
     });
   });
 
